012222ee323307c3bbe7f36b1cbdfe8b79cba480
VinuelMancent <61296588+VinuelMancent@users.noreply.github.com>
2021-02-13 11:56:07 +0100

    initial commit

---
+++ Logger/Src/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Serilog;
+
+namespace Logger.Src
+{
+    public class Logger
+    {
+        private Serilog.Core.Logger log;
+        private string channelName = "";
+        //Constructor
+        #nullable enable
+        public Logger(string channelName)
+        {
+            this.channelName = channelName;
+            
+            log = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+        //Constructor for logging into a file
+        public Logger(string channelName, string filePath)
+        {
+            this.channelName = channelName;
+            log = new LoggerConfiguration()
+                .WriteTo.File(filePath)
+                .CreateLogger();
+           
+        }
+    }
+}
---
+++ Logger/Src/Manager.cs
@@ -0,0 +1,7 @@
+namespace Logger.Src
+{
+    public class Manager
+    {
+        
+    }
+}
